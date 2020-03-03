2a614e47839a212ef3266807b199199275f05da3
ViktoriaGatz <ledneva.margo@gmail.com>
2020-03-03 14:19:36 +0700

    Вариант полного удаление всех образов и контейнеров

--- dockerRm.sh
+++ dockerRm.sh
@@ -1,5 +1,18 @@
 #/bin/bash
 
+# Удалить всё
+
+# docker stop $(docker ps -a -q)
+
+# docker rm $(docker ps -a -q)
+
+# yes| docker container prune
+
+# docker rmi $(docker images -q)
+
+# docker images -q |xargs docker rmi
+
+# Удалить только нужное
  docker stop mysql-standalone4
 
  docker stop spring-rest-service-0.1.0
