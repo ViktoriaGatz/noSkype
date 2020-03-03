f0c72ab9517346ce7f16208fb72241601aa2cd45
ViktoriaGatz <ledneva.margo@gmail.com>
2020-03-03 14:17:12 +0700

    Добавлена часть скрипта для удаления контейнеров

--- docker.sh
+++ docker.sh
@@ -15,3 +15,10 @@ docker run -p 8080:8080 --name spring-rest-service-0.1.0 --link mysql-standalone
 docker start spring-rest-service-0.1.0
 
 docker logs spring-rest-service-0.1.0
+
+
+# docker stop mysql-standalone4
+# docker stop spring-rest-service-0.1.0
+# docker rm mysql-standalone4
+# docker rm spring-rest-service-0.1.0
+# docker image rm spring-rest-service-0.1.0:latest
